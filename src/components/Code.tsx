48eee2794f181618f056d0aa48bf130422ab7815
TheRedMine TheredMaths <theredminedu51@gmail.com>
2023-02-19 14:14:24 +0100

    Code component (Prismjs)

---
+++ src/components/Code.tsx
@@ -0,0 +1,22 @@
+import { Code as CodeTypes } from '../types/ComponentsTypes';
+
+import { useEffect } from 'react';
+import { highlightAll } from 'prismjs';
+
+const Code = ({
+    children, language = "html", theme = "okaidia"
+} : CodeTypes.Types) => {
+    useEffect(() => highlightAll());
+
+    require(`prismjs/themes/prism${theme==="default"?"":"-".concat(theme)}.min.css`);
+
+    return (
+        <pre>
+            <code className={`language-${language}`}>
+                {children?.toString()}
+            </code>
+        </pre>
+    );
+};
+
+export default Code;
